58a6797c8c4f4eeb0a9e17ed7ce271a28db6b9cf
PANKYAAA <pankajs.gaikar@gmail.com>
2019-10-30 17:40:10 +0530

    Initial Commit

---
+++ NYTimes Top StoriesTests/NYTimes_Top_StoriesTests.swift
@@ -0,0 +1,34 @@
+//
+//  NYTimes_Top_StoriesTests.swift
+//  NYTimes Top StoriesTests
+//
+//  Created by Pankaj Gaikar on 30/10/19.
+//  Copyright © 2019 Pankaj Gaikar. All rights reserved.
+//
+
+import XCTest
+@testable import NYTimes_Top_Stories
+
+class NYTimes_Top_StoriesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
